5be97d6e7a136ef1ad6a0a4b1f76581ac79a1b45
Spoorthy1423 <142025923+Spoorthy1423@users.noreply.github.com>
2024-06-28 10:33:11 +0530

    Add files via upload

--- sst-classes-master/src/index.js
+++ sst-classes-master/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from "redux";
+import { combineReducers } from "redux";
+import cartReducer from "./Cart";
+import categoriesReducer from "./Categories";
+import thunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-      <App />
-  </Provider>
-);
+const reducer = combineReducers({
+    cart: cartReducer,
+    categories: categoriesReducer
+})
+const store = createStore(reducer, applyMiddleware(thunk));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+export default store;
